646fc296844dbc1f40f360e114d124fd49129743
pangzixiang <65987771+pangzixiang@users.noreply.github.com>
2020-07-20 15:54:23 +0800

    update

---
+++ src/main/java/platform/Contract.java
@@ -0,0 +1,28 @@
+package platform;
+import org.hyperledger.fabric.contract.Context;
+import org.hyperledger.fabric.contract.ContractInterface;
+import org.hyperledger.fabric.contract.annotation.*;
+import org.hyperledger.fabric.shim.ChaincodeException;
+import org.hyperledger.fabric.shim.ChaincodeStub;
+/**
+ * Class: Contract
+ */
+@org.hyperledger.fabric.contract.annotation.Contract(
+        name = "platform.Contract",
+        info = @Info(
+                title = "Contract",
+                description = "SmartContract for platform",
+                version = "1.0.0",
+                license = @License(
+                        name = "Apache 2.0 License",
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.html"),
+                contact = @Contact(
+                        email = "313227220@qq.com",
+                        name = "Group3"
+                )
+        )
+)
+@Default
+public final class Contract implements ContractInterface{
+
+}
