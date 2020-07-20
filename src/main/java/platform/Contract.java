ed5cfd8669a5740e0d917e6b105b8f9073ab1a88
pangzixiang <65987771+pangzixiang@users.noreply.github.com>
2020-07-20 16:33:23 +0800

    update

--- src/main/java/platform/Contract.java
+++ src/main/java/platform/Contract.java
@@ -25,6 +25,18 @@ import org.hyperledger.fabric.shim.ChaincodeStub;
 @Default
 public final class Contract implements ContractInterface{
     enum Message {
+        ARG_NUM_WRONG("Incorrect number of arguments '%s'");
+
+
+        private String tmpl;
+
+        private Message(String tmpl) {
+            this.tmpl = tmpl;
+        }
+
+        public String template() {
+            return this.tmpl;
+        }
 
     }
 
@@ -32,5 +44,28 @@ public final class Contract implements ContractInterface{
     public void initCredit(final Context ctx, final String userID){
         ChaincodeStub stub = ctx.getStub();
         stub.putStringState(userID+"-Credit", "100");
+    }
+
+    @Transaction(name = "ChangeCredit", intent = Transaction.TYPE.SUBMIT)
+    public void changeCredit(final Context ctx, final String userID, final String changeValue){
+        ChaincodeStub stub = ctx.getStub();
+        String oldCredit = stub.getStringState(userID+"-Credit");
+        try {
+            Integer.valueOf(changeValue);
+        }catch (Exception e){
+            String errorMessage = String.format(Message.ARG_NUM_WRONG.template(),changeValue);
+            System.out.println(errorMessage);
+            throw new ChaincodeException(errorMessage,e);
+        }
+
+        int newCredit = Integer.parseInt(oldCredit) + Integer.parseInt(changeValue);
+        if (newCredit < 0){
+            String errorMessage = String.format(Message.ARG_NUM_WRONG.template(),changeValue);
+            System.out.println(errorMessage);
+            throw new ChaincodeException(errorMessage);
+        }
+
+        stub.putStringState(userID+"-Credit",String.valueOf(newCredit));
+
     }
 }
