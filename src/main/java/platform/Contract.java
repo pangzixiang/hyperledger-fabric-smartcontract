a6d5c5d47dc97acbe946e3a3ddd2df384808bc49
pangzixiang <65987771+pangzixiang@users.noreply.github.com>
2020-07-20 18:54:21 +0800

    update

--- src/main/java/platform/Contract.java
+++ src/main/java/platform/Contract.java
@@ -1,10 +1,15 @@
 package platform;
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.hyperledger.fabric.contract.Context;
 import org.hyperledger.fabric.contract.ContractInterface;
 import org.hyperledger.fabric.contract.annotation.*;
 import org.hyperledger.fabric.shim.ChaincodeException;
 import org.hyperledger.fabric.shim.ChaincodeStub;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Class: Contract
  */
@@ -29,10 +34,10 @@ public final class Contract implements ContractInterface {
         ARG_NUM_WRONG("Incorrect number of arguments '%s'"),
         USER_NOT_EXISTING("User '%s' does not exist."),
         RULE_NOT_EXIST("rule '%s' not exist"),
-        GROUP_BUYING_NOT_EXIST("this group buying order '%s' not exist");
-        GROUP_BUYING_NOT_SUCCESS("this group buying order '%s' not success");
-        Transaction_ERROR("Group buying order '%s' not belong to rule '%s'");
-        Transaction_NOT_EXIST("this transaction '%s' not exist")
+        GROUP_BUYING_NOT_EXIST("this group buying order '%s' not exist"),
+        GROUP_BUYING_NOT_SUCCESS("this group buying order '%s' not success"),
+        Transaction_ERROR("Group buying order '%s' not belong to rule '%s'"),
+        Transaction_NOT_EXIST("this transaction '%s' not exist");
         private String tmpl;
 
         private Message(String tmpl) {
@@ -98,22 +103,22 @@ public final class Contract implements ContractInterface {
                 String payerIDs = groupBuying.getString("userID");
                 String payments = discountRule.getString("firstBuyerPrice");
                 int receivables = Integer.parseInt(discountRule.getString("firstBuyerPrice"));
-                for(int i =2;i<=Integer.parseInt(groupBuying.getString("groupNum");i++)){
+                for(int i =2;i<=Integer.parseInt(groupBuying.getString("groupNum"));i++){
                     String participateBuyingString = stub.getStringState(groupBuyingID+"-"+String.valueOf(i));
                     JSONObject participateBuying = JSONObject.parseObject(participateBuyingString);
-                    payerIDs += "/" + participateBuying..getString("userID");
+                    payerIDs += "/" + participateBuying.getString("userID");
                     payments += "/" + discountRule.getString("otherBuyerPrice");
                     receivables += Integer.parseInt(discountRule.getString("otherBuyerPrice"));
                 }
-                String payerIDs = discountRule.getString("sellerID");
+                String sellerIDs = discountRule.getString("sellerID");
                 Map<String,String> map = new HashMap<>();
                 map.put("transState","0");   //0代表支付待完成 1代表支付已完成 -1代表违约
                 map.put("payerIDs", payerIDs);
-                map.put("payee",sellerID);
+                map.put("payee",sellerIDs);
                 map.put("payments",payments);
-                map.put("receivables",receivables);
+                map.put("receivables",String.valueOf(receivables));
                 //创建交易单
-                stub.putStringState(groupBuyingID+"-"+discountRuleID,JSON.toJSONString(map));
+                stub.putStringState(groupBuyingID+"-"+discountRuleID, JSON.toJSONString(map));
                 
             }else{
                 String errorMessage = String.format(Message.Transaction_ERROR.template(), groupBuyingID, discountRuleID);
@@ -130,7 +135,7 @@ public final class Contract implements ContractInterface {
     }
 
     @Transaction(name = "ChangeTrans", intent = Transaction.TYPE.SUBMIT)
-    public String queryCredit(final Context ctx, final String transID, final String transState) {
+    public void changeTrans(final Context ctx, final String transID, final String transState) {
         ChaincodeStub stub = ctx.getStub();
         String transactionString = stub.getStringState(transID);
         if (transactionString.isEmpty()) {
@@ -143,7 +148,7 @@ public final class Contract implements ContractInterface {
     }
 
     @Transaction(name = "QueryTrans", intent = Transaction.TYPE.EVALUATE)
-    public String queryCredit(final Context ctx, final String transID) {
+    public String queryTrans(final Context ctx, final String transID) {
         ChaincodeStub stub = ctx.getStub();
         String transactionString = stub.getStringState(transID);
         if (transactionString.isEmpty()) {
